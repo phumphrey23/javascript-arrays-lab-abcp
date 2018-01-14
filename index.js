2ca30856e947318805eb0fb3d17a6bd7a8aad01f
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-14 20:11:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name){
-  kittens.push(name);
+  kittens.push('Ralph');
   return kittens;
 };
 function destructivelyPrependKitten(name){
-  kittens.unshift(name);
+  kittens.unshift('Bob');
   return kittens;
 }
