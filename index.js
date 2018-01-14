43831de93a6588d9d2a17fb4b612ed63c3e1a521
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-14 20:46:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph');
+  return kittens;
+};
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob');
+  return kittens;
+};
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+};
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+};
+function appendKitten(name){
+  var addEndKittens = [...kittens,'Broom'];
+  return addEndKittens;
+};
+function prependKitten(name){
+  var addStrtKittens = ['Arnold',...kittens];
+  return addStrtKittens;
+};
+function removeLastKitten(){
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+};
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1);
+  return newKittens;
+};
